269b03cb9208b08c155fd849e22fff46b5f8eda6
hiruna <hiruna.kawinda@gmail.com>
2025-03-18 00:56:00 +1100

    add timestamp to output dir

--- pico_rootfs/build.sh
+++ pico_rootfs/build.sh
@@ -1,16 +1,49 @@
-#!/bin/bash
+#!/bin/bash -e
 
 #
 # SPDX-FileCopyrightText: © 2025 Hiruna Wijesinghe <hiruna.kawinda@gmail.com>
 # SPDX-License-Identifier: GPL-2.0-only
 #
 
+function split_tar() {
+    local tar_dir="$1"
+    local tar_filename="$2"
+    local split_size="50M"
+    local num_files="$(du -sh "$folder_path" | awk '{print $1/50}')"
+
+    if [[ ! -d ${tar_dir} ]]; then
+      echo "directory ${tar_dir} does not exist!"
+      exit 1
+    fi
+    if [[ ! -f "${tar_dir}/${tar_filename}" ]]; then
+      echo "file ${tar_dir}/${tar_filename} does not exist!"
+      exit 1
+    fi
+    cwd=$(pwd)
+    cd "${tar_dir}"
+
+    echo "Calculating md5 for each split package..."
+    md5sum "./${tar_filename}" > "./${tar_filename}.md5"
+
+    echo "Splitting folder $folder_path into $num_files packages of size $split_size..."
+    split -b "$split_size" -d -a 1 "./${tar_filename}" "./${tar_filename}.split."
+
+    echo "Split and md5 calculation completed."
+
+    cd "${cwd}"
+}
+
 UBUNTU_VERSION=22.04.3
 UBUNTU_BASE_TAR_FILENAME="ubuntu-base-${UBUNTU_VERSION}-base-armhf.tar.gz"
 PICO_ROOTFS_IMAGE_TAG="pico_rootfs_ubuntu:${UBUNTU_VERSION}"
-OUTPUT_DIR="./output"
+OUTPUT_DIR="./output/$(date '+%Y-%m-%d_%H%M%S')"
 OUTPUT_TAR_GZ_FILENAME="luckfox-ubuntu-${UBUNTU_VERSION}.tar.gz"
-TEMP_ROOTFS_DIR=$(mktemp -d --suffix pico_rootfs -p output/)
+
+if [[ ! -f ${OUTPUT_DIR} ]]; then
+  mkdir -p "${OUTPUT_DIR}"
+fi
+
+TEMP_ROOTFS_DIR=$(mktemp -d --suffix pico_rootfs -p "${OUTPUT_DIR}/")
 
 if [[ ! -f ${UBUNTU_BASE_TAR_FILENAME} ]]; then
   echo "Downloading ${UBUNTU_BASE_TAR_FILENAME}..."
@@ -24,3 +57,5 @@ docker buildx build --platform linux/arm/v7 -t ${PICO_ROOTFS_IMAGE_TAG} \
 tar cvzf "${OUTPUT_DIR}/${OUTPUT_TAR_GZ_FILENAME}" ${TEMP_ROOTFS_DIR}
 
 rm -rf ${TEMP_ROOTFS_DIR}
+
+# split_tar "${OUTPUT_DIR}" "${OUTPUT_TAR_GZ_FILENAME}"
